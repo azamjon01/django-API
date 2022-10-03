f94332d4660713e5736453fc9c7c3fe32c42ec52
azamjon01 <ziyobiddionovazamjon01@gmail.com>
2022-10-03 14:51:28 +0500

    main

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, krosovkaMakeAPI
+from .views import home, krosovkaMakeAPI, singleAPI
 
 app_name = 'api'
 
 urlpatterns = [
     path('', home, name="home"),
-    path('krosovka-api/', krosovkaMakeAPI)
+    path('krosovka-api/', krosovkaMakeAPI),
+    path('krosovka-api/<int:pk>/', singleAPI)
+
 ]
