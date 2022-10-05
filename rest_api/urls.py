858daf2a1c211bd7922a4eff939975d69e22b29f
azamjon01 <ziyobiddionovazamjon01@gmail.com>
2022-10-05 14:15:13 +0500

    main

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import home, krosovkaMakeAPI, singleAPI
+from .views import home, krosovkaMakeAPI, malumotDelete, malumotJoylash, malumotUpdate, singleAPI
 
 app_name = 'api'
 
 urlpatterns = [
     path('', home, name="home"),
     path('krosovka-api/', krosovkaMakeAPI),
-    path('krosovka-api/<int:pk>/', singleAPI)
-
+    path('krosovka-api/<int:pk>/', singleAPI),
+    path('create/' , malumotJoylash),
+    path('create/<int:pk>/', malumotUpdate),
+    path('delete/', malumotDelete),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -8,6 +8,7 @@ from .models import *
 def home(request):
     return render(request, 'home.html')
 
+# API tolq chiqarish
 @api_view(["GET"])
 @permission_classes((permissions.AllowAny, ))
 def krosovkaMakeAPI(request):
@@ -15,10 +16,44 @@ def krosovkaMakeAPI(request):
     serializer = KrosovkaAPI(krosovka, many=True)
     return Response(serializer.data)
 
-
+# api ni id orqali (alohida, ALOHIDA) CHIQARISH
 @api_view(["GET"])
 @permission_classes((permissions.AllowAny, ))
 def singleAPI(request, pk):
     krosovka = Krosovka.objects.get(id=pk)
     serializer = KrosovkaAPI(krosovka, many=False)
     return Response(serializer.data)
+
+# post joylash (malumot joylash)    
+@api_view(["POST"])
+@permission_classes((permissions.AllowAny, ))
+def malumotJoylash(request):
+    krosovka = Krosovka.objects.get(id)
+    serializer = KrosovkaAPI(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+  
+# post joylash Update qilish    
+@api_view(["POST"])
+@permission_classes((permissions.AllowAny, ))
+def malumotUpdate(request, pk):
+    krosovka = Krosovka.objects.get(id=pk)
+    serializer = KrosovkaAPI(instance=krosovka, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+# post delete qilish
+@api_view(["DELETE"])
+@permission_classes((permissions.AllowAny, ))
+def malumotDelete(request, pk):
+    krosovka = Krosovka.objects.get(id=pk)
+    
+
+    
+    return Response('Mufaqiyatli ochirildi')    
